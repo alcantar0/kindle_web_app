10d50064cd602d9254485fed5928c4d35b482511
pedro <alcantarapedro69@yahoo.com>
2022-08-04 09:39:11 -0300

    pypro

--- core/views.py
+++ core/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, HttpResponse
 
 
 
 def index(request):
-    return redirect('/home')
+    return redirect('home')
 
 def upload(request):
     return render(request, "core/login.html")
@@ -12,4 +11,6 @@ def upload(request):
 def process_text(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['documento']
-        print(uploaded_file.read())
+        a = uploaded_file.read().decode('UTF-8')
+        print(a)
+        return HttpResponse('<h1> OK </h1>')
