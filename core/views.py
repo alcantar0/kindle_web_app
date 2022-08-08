97fdcc14d225c3d9f6fe3d08951d1d2a01670a7e
pedro <alcantarapedro69@yahoo.com>
2022-08-08 12:01:57 -0300

    mostrando highlights pelo template page

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 """Conjunto de views para o app core"""
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
 
 
 def index():
@@ -19,7 +19,7 @@ def process_text(request):
         livro_nome = request.POST.get("titulo")
         raw = uploaded_file.read().decode("UTF-8")
         raw_dividido = raw.split("==========")
-
+        list_dicts = []
         for highlight in raw_dividido:
             if livro_nome not in highlight:
                 continue
@@ -27,7 +27,10 @@ def process_text(request):
             ind = separado[2].find("Added on ")
             data_pronta = separado[2]
             data_pronta = data_pronta[ind + 9 :]
-            print(separado[1])
-            print(separado[2])  # Titulo livro
-            print(separado[4])  # Highlight
-    return HttpResponse("<h1>OK</h1>")
+            # print(separado[1])
+            # print(separado[2])  # Titulo livro
+            # print(separado[4])  # Highlight
+            dict_high = {"highlight": separado[4], "data": data_pronta}
+            list_dicts.append(dict_high)
+        data = {"highlights": list_dicts}
+    return render(request, "core/page.html", data)
