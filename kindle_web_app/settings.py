ea80f63567c0b7732beedfd584ecc45a00e49043
Pedro Henrique de Sousa Alcantara <50456948+alcantar0@users.noreply.github.com>
2023-01-09 12:21:48 -0300

    Update settings.py

--- kindle_web_app/settings.py
+++ kindle_web_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["kindle-web-app.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
