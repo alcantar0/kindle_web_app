54207028d22e52bc5034d2eb5ffa931f1c8d98ff
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2023-01-09 15:12:46 -0300

    Anotações nao feitas

--- kindle_web_app/settings.py
+++ kindle_web_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["web-production-13ac.up.railway.app", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
