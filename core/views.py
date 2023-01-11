28c1ad91ceda11645863448a1275a0442f69b682
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2023-01-11 12:11:54 -0300

    Algoritmo para anotações funcionando e pronto

--- core/views.py
+++ core/views.py
@@ -20,51 +20,59 @@ def process_text(request):  # pylint: disable=R0914 R1710
         uploaded_file = request.FILES["documento"]
         print(request.FILES)
         raw = uploaded_file.read().decode("UTF-8")
-        # livro_nome = "Scar Tissue"
-        raw_dividido = raw.split("==========")
-        list_dicts = []
+        raw = raw.split("\n")
+        counter = 0
+        see = True
+        mark = []
         titles = []
-        counter = 1
-        for highlight in raw_dividido:
-            highlight = highlight.strip("\r")
-            highlight = highlight.strip("")
-            separado = highlight.split("\n")
-            if separado == [
-                "",
-                "",
-            ]:  # Tratamento de erro para a última linha do arquivo
-                continue
 
-            titulo = ""
-            highlight = ""
-            if counter == 1:  # Tratamento de erro para primeira linha do arquivo
-                ind = separado[1].find("Added on ")
-                string_data_raw = separado[1]
-                titulo = separado[0]
-                highlight = separado[3]
-
-                titles.append(f"{separado[0]}")
-                print(f"{separado[0]}")
+        while see is True:
+            try:
+                data = raw[counter + 1]
+            except IndexError:
+                see = False
+                break
+            if data[0:8] == "- Your N":
+                if raw[counter] not in titles:
+                    titles.append(raw[counter])
+                ind = raw[counter + 1].find("Added on ")
+                string_data_raw = raw[counter + 1]
+                string_data_raw = string_data_raw[ind + 9 :]
+                data = proccess_data.transform_to_list(string_data_raw)
+                data_pronta = proccess_data.convert_to_string(*data)
+                dicionario = {
+                    "titulo": raw[counter],
+                    "data": data_pronta,
+                    "highlight": raw[counter + 8],
+                    "anotacao": raw[counter + 3],
+                }
+                counter += 10
+                mark.append(dicionario)
+                if not Livro.objects.filter(  # pylint: disable=E1101
+                    **dicionario
+                ).exists():  # pylint: disable=E1101
+                    Livro.objects.create(**dicionario)  # pylint: disable=E1101
             else:
-                if separado[1] not in titles:
-                    titles.append(separado[1])
-                ind = separado[2].find("Added on ")
-                titulo = separado[1]
-                highlight = separado[4]
-                string_data_raw = separado[2]
-            string_data_raw = string_data_raw[ind + 9 :]
-            # print(string_data_raw)
+                if raw[counter] not in titles:
+                    titles.append(raw[counter])
+                ind = raw[counter + 1].find("Added on ")
+                string_data_raw = raw[counter + 1]
+                string_data_raw = string_data_raw[ind + 9 :]
+                data = proccess_data.transform_to_list(string_data_raw)
+                data_pronta = proccess_data.convert_to_string(*data)
 
-            # print(titulo)  # Titulo
-            # print(separado[2])  # Dados
-            # print(separado[4])  # Highlight
-            data = proccess_data.transform_to_list(string_data_raw)
-            data_pronta = proccess_data.convert_to_string(*data)
-            dict_high = {"titulo": titulo, "data": data_pronta, "highlight": highlight}
-            if not Livro.objects.filter(**dict_high).exists():  # pylint: disable=E1101
-                Livro.objects.create(**dict_high)  # pylint: disable=E1101
-            list_dicts.append(dict_high)
-            counter += 1
+                dicionario = {
+                    "titulo": raw[counter],
+                    "data": data_pronta,
+                    "highlight": raw[counter + 3],
+                    "anotacao": "0",
+                }
+                counter += 5
+                mark.append(dicionario)
+                if not Livro.objects.filter(  # pylint: disable=E1101
+                    **dicionario
+                ).exists():  # pylint: disable=E1101
+                    Livro.objects.create(**dicionario)  # pylint: disable=E1101
         titles = Livro.objects.all().distinct("titulo")  # pylint: disable=E1101
         return render(request, "core/teste.html", {"titles": titles})
 
--- kindle_web_app/settings.py
+++ kindle_web_app/settings.py
@@ -41,8 +41,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "dal",
-    "dal_select2",
 ]
 
 MIDDLEWARE = [
