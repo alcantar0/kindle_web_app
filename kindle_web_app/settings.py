4226195be247155833c009b25cd8f61ae431c033
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2023-01-13 17:39:56 -0300

    ok

--- kindle_web_app/settings.py
+++ kindle_web_app/settings.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
