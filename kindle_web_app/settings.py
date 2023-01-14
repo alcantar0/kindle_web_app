f66e298c358cb9361b8f5109e184f25fdf663013
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2023-01-14 19:21:19 -0300

    old removed

--- kindle_web_app/settings.py
+++ kindle_web_app/settings.py
@@ -28,11 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+"""
 if os.environ.get("DJANGO_ENVIROMENT") == "production":
     DEBUG = False
 else:
     DEBUG = True
-
+"""
+DEBUG = False
 ALLOWED_HOSTS = ["web-production-78f6.up.railway.app", "127.0.0.1"]
 # Application definition
 
@@ -135,7 +137,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "static/"
 
