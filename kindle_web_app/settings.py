a65d74e17a7929ad3886c98cef59f6859004d919
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2023-01-14 18:52:02 -0300

    false

--- kindle_web_app/settings.py
+++ kindle_web_app/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["web-production-78f6.up.railway.app", "127.0.0.1"]
 # Application definition
