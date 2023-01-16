e840d73626bd92e5de496e537127276d80727601
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2023-01-16 11:38:15 -0300

    using django sessions

--- core/views.py
+++ core/views.py
@@ -1,13 +1,9 @@
 """Conjunto de views para o app core"""
 from django.shortcuts import render
 
-from django.db import connection
-
 from django.db import transaction
 
 from core.utils import proccess_data
-
-from core.models import Livro
 
 
 def dictfetchall(cursor):
@@ -112,91 +108,9 @@ def process_text(request):  # pylint: disable=R0914 R1710 R0912 R0915
                 mark.append(dicionario)
             else:
                 counter += 1
-            cursor = connection.cursor()
-            cursor.execute(
-                """INSERT INTO core_livro(titulo, data, highlight, anotacao)
-                VALUES (%s, %s, %s, %s);""",
-                [
-                    dicionario["titulo"],
-                    dicionario["data"],
-                    dicionario["highlight"],
-                    dicionario["anotacao"],
-                ],
-            )  # pylint: disable=C0301 )
-        # cursor.execute("SELECT DISTINCT titulo FROM core_livro;")
-        # titles = dictfetchall(cursor)
-        print(titles)
-        return render(request, "core/teste.html", {"titles": titles})
 
+        request.session["livros"] = mark
 
-@transaction.non_atomic_requests
-def process_text_pt(request):  # pylint: disable=R0914 R1710 R0912 R0915
-    """Função que processa e retorna os highlights do arquivo"""
-    if request.method == "POST":  # pylint: disable=R1710
-        uploaded_file = request.FILES["documento"]
-        raw = uploaded_file.read().decode("UTF-8")
-        raw = raw.split("\n")
-
-        raw = [element for element in raw if (element and element.strip())]
-
-        counter = 0
-        see = True
-        mark = []
-        titles = []
-        dicionario = {}
-        while see is True:
-            try:
-                data = raw[counter + 1]
-            except IndexError:
-                see = False
-                break
-            if data[0:8] == "- Your B":
-                counter += 3
-                continue
-
-            if data[0:8] == "- Your N":
-                if raw[counter] not in titles:
-                    titles.append(raw[counter])
-                ind = raw[counter + 1].find("Added on ")
-                string_data_raw = raw[counter + 1]
-                string_data_raw = string_data_raw[ind + 9 :]
-                data = proccess_data.transform_to_list(string_data_raw)
-                data_pronta = proccess_data.convert_to_string(*data)
-                dicionario = {
-                    "titulo": raw[counter],
-                    "data": data_pronta,
-                    "highlight": raw[counter + 6],
-                    "anotacao": raw[counter + 2],
-                }
-                counter += 8
-                mark.append(dicionario)
-
-            elif data[0:8] == "- Your H":
-                if raw[counter] not in titles:
-                    titles.append(raw[counter])
-                ind = raw[counter + 1].find("Added on ")
-                string_data_raw = raw[counter + 1]
-                string_data_raw = string_data_raw[ind + 9 :]
-                data = proccess_data.transform_to_list(string_data_raw)
-                data_pronta = proccess_data.convert_to_string(*data)
-
-                dicionario = {
-                    "titulo": raw[counter],
-                    "data": data_pronta,
-                    "highlight": raw[counter + 2],
-                    "anotacao": "0",
-                }
-                counter += 4
-                mark.append(dicionario)
-                Livro.objects.create(**dicionario)  # pylint: disable=E1101
-            else:
-                counter += 1
-            if not Livro.objects.filter(**dicionario).exists():  # pylint: disable=E1101
-                cursor = connection.cursor()
-                cursor.execute(
-                    f"INSERT INTO core_livro(titulo, data, highlight, anotacao)VALUES ({dicionario['titulo']}, {dicionario['data']}, {dicionario['highlight']}, {dicionario['anotacao']},);"  # pylint: disable=C0301
-                )
-        titles = Livro.objects.all().distinct("titulo")  # pylint: disable=E1101
         return render(request, "core/teste.html", {"titles": titles})
 
 
@@ -204,10 +118,9 @@ def process_text_pt(request):  # pylint: disable=R0914 R1710 R0912 R0915
 def listar(request):  # pylint: disable=R1710
     """Lista os highlights"""
     if request.method == "POST":
+        raw = request.session.get("livros")
         titulo = request.POST.get("nome_livro")
-        titles = Livro.objects.filter(titulo__contains=titulo)  # pylint: disable=E1101
-        titles = list(titles)
 
-        cursor = connection.cursor()
-        cursor.execute("TRUNCATE TABLE core_livro")
+        titles = [element for element in raw if titulo in element["titulo"]]
+
         return render(request, "core/page.html", {"dados": titles})
