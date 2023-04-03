5c99f2fdd46bb549b8f73e07f35e072fc6b6c06a
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2023-04-03 20:47:44 -0300

    setuptools

--- kindle_web_app/settings.py
+++ kindle_web_app/settings.py
@@ -81,8 +81,6 @@ WSGI_APPLICATION = "kindle_web_app.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-"""
-
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -90,7 +88,7 @@ DATABASES = {
     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 """
-#
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
@@ -101,7 +99,7 @@ DATABASES = {
         "PORT": os.getenv("PGPORT"),
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
