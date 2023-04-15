edf8959080231605790090743ad3a7117422336f
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2023-04-15 14:19:57 -0300

    dotenv

--- kindle_web_app/settings.py
+++ kindle_web_app/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv("SECRET_KEY")
-SECRET_KEY = "(hx_n3I@$W!ZV!uJ$_s~B]Wjd{A4YKz;rJDF+F97h#NhUv&CL8"
+SECRET_KEY = os.getenv("SECRET_KEY")
+# SECRET_KEY = "(hx_n3I@$W!ZV!uJ$_s~B]Wjd{A4YKz;rJDF+F97h#NhUv&CL8"
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get("DJANGO_ENVIROMENT") == "production":
 
